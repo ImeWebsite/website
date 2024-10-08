e8eb233711663eb3c0eec86e2772d04635f3c8bf
Janin Michel-Mathias <107043504+Janin-Michel-Mathias@users.noreply.github.com>
2024-10-08 21:33:36 +0200

    Update GrimpeFamille.tsx

--- src/components/Inscription/GrimpeFamille.tsx
+++ src/components/Inscription/GrimpeFamille.tsx
@@ -5,7 +5,7 @@ const GrimpeFamille = () => {
   return (
     <div className={style.container}>
         <h1 className={style.title}>Pour les enfants et la grimpe famille</h1>
-        <p className={style.text}>L’inscription en ligne n’est pas possible, merci de nous contacter par mail à <b>ivrymontagneescalalde@gmail.com</b></p>
+        <p className={style.text}>L’inscription en ligne n’est pas possible, merci de nous contacter par mail à <b>ivrymontagneescalade@gmail.com</b></p>
     </div>
   )
 }
