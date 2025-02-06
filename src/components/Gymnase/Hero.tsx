16f65337bfe57a273963e916b3094bc3a2bbc954
Janin-Michel-Mathias <micheljanin296@gmail.com>
2025-02-06 20:43:11 +0100

    Fix typo

--- src/components/Gymnase/Hero.tsx
+++ src/components/Gymnase/Hero.tsx
@@ -5,7 +5,7 @@ import style from './hero.module.css'
 const Hero = () => {
   return (
     <div className={style.container}>
-        <h1>Le mur d'escalade</h1>
+        <h1>Le mur d&apos;escalade</h1>
         <h2>Du collège Gisèle Halimi</h2>
         <a href="https://maps.app.goo.gl/Y79iYwujnhKhrg53A"><b>12, rue du 19 mars 1962<br /> Quartier : Plateau-Monmousseau</b><br/> Tram T9 – Arrêt « La Briqueterie »</a>
         <div className={style.img}></div>
